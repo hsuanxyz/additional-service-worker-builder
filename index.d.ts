65bd9dd29b038ca9fc7c0fefb66fd1ba8df91c83
Hsuan Lee <hsuangm@gmail.com>
2020-09-29 11:34:02 +0800

    chore: update .gitignore

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { JsonObject } from '@angular-devkit/core';
-interface Options extends JsonObject {
-    browserTarget: string;
-}
-declare const _default: import("@angular-devkit/architect/src/internal").Builder<Options>;
-export default _default;
